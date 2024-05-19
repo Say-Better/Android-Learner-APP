c793381e64b85f79d0679c8b34f58e610ac31b94
yeongin-ji <jyi0106@gachon.ac.kr>
2024-05-19 10:41:56 +0900

    Feat: Start Service func

--- app/src/main/java/gdsc/solutionchallenge/saybetter/saybetterlearner/service/MainService.kt
+++ app/src/main/java/gdsc/solutionchallenge/saybetter/saybetterlearner/service/MainService.kt
@@ -4,10 +4,26 @@ import android.app.Service
 import android.content.Intent
 import android.os.IBinder
 import dagger.hilt.android.AndroidEntryPoint
+import gdsc.solutionchallenge.saybetter.saybetterlearner.service.MainServiceActions.START_SERVICE
 
 
 @AndroidEntryPoint
 class MainService : Service() {
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        intent?.let { incomingIntent ->
+            when(incomingIntent.action) {
+                START_SERVICE.name -> handleStartService(incomingIntent)
+                else -> Unit
+            }
+        }
+        return START_STICKY
+    }
+
+    private fun handleStartService(incomingIntent: Intent) {
+        TODO("Not yet implemented")
+    }
+
     override fun onBind(intent: Intent?): IBinder? {
         TODO("Not yet implemented")
         return null
