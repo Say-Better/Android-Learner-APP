b1b07182ce940210f8e4fc2786c0738dc2bdf2dd
yeongin-ji <jyi0106@gachon.ac.kr>
2024-06-13 10:07:07 +0900

    commit2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -81,6 +81,7 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
 
     //바텀네비
     implementation ("androidx.navigation:navigation-compose:2.7.6")
