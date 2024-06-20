b378f3771932a096120cac9b8d226ee8aa899a45
yeongin-ji <jyi0106@gachon.ac.kr>
2024-06-20 21:51:00 +0900

    ✨ Feat and close #8 - 화상통화 이슈 닫고 다시 생성
    webrtc 관련 이슈 새로 파겠습니다.

--- app/src/main/java/gdsc/solutionchallenge/saybetter/saybetterlearner/ui/videocall/VideoCallActivity.kt
+++ app/src/main/java/gdsc/solutionchallenge/saybetter/saybetterlearner/ui/videocall/VideoCallActivity.kt
@@ -41,6 +41,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import dagger.hilt.android.AndroidEntryPoint
 import gdsc.solutionchallenge.saybetter.saybetterlearner.R
 import gdsc.solutionchallenge.saybetter.saybetterlearner.model.data.local.entity.Symbol
 import gdsc.solutionchallenge.saybetter.saybetterlearner.ui.component.SymbolLayout.Symbol
@@ -53,9 +54,13 @@ import gdsc.solutionchallenge.saybetter.saybetterlearner.ui.theme.GrayW40
 import gdsc.solutionchallenge.saybetter.saybetterlearner.ui.theme.MainGreen
 import gdsc.solutionchallenge.saybetter.saybetterlearner.ui.theme.Red
 import gdsc.solutionchallenge.saybetter.saybetterlearner.utils.Customclick.CustomClickEvent
+import gdsc.solutionchallenge.saybetter.saybetterlearner.utils.webrtc.service.MainService
+import gdsc.solutionchallenge.saybetter.saybetterlearner.utils.webrtc.service.MainServiceRepository
+import javax.inject.Inject
 
 const val TAG = "VideoCall"
 
+@AndroidEntryPoint
 class VideoCallActivity : ComponentActivity(), TTSListener {
 
     private lateinit var ttsManager: TTSManager
@@ -63,6 +68,8 @@ class VideoCallActivity : ComponentActivity(), TTSListener {
 
     private var target: String? = null
     private var isCaller: Boolean = true
+
+    @Inject lateinit var serviceRepository: MainServiceRepository
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -81,8 +88,9 @@ class VideoCallActivity : ComponentActivity(), TTSListener {
         }
 
         isCaller = intent.getBooleanExtra("isCaller", true)
-
         Log.d(TAG, "target: $target\nisCaller: $isCaller")
+
+//        serviceRepository.setupViews()
 
     }
 
--- app/src/main/java/gdsc/solutionchallenge/saybetter/saybetterlearner/utils/webrtc/webrtcClient/WebRTCClient.kt
+++ app/src/main/java/gdsc/solutionchallenge/saybetter/saybetterlearner/utils/webrtc/webrtcClient/WebRTCClient.kt
@@ -1,7 +1,8 @@
 package gdsc.solutionchallenge.saybetter.saybetterlearner.utils.webrtc.webrtcClient
-
+//
 import android.content.Context
 import com.google.gson.Gson
+import gdsc.solutionchallenge.saybetter.saybetterlearner.model.remote.dto.DataModel
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -10,4 +11,12 @@ class WebRTCClient @Inject constructor(
     private val context : Context,
     private val gson : Gson
 ) {
+//    val listener: Listener? = null
+
+    private fun
+
+    interface Listener {
+        fun onTransferEventToSocket(data: DataModel)
+    }
+
 }
