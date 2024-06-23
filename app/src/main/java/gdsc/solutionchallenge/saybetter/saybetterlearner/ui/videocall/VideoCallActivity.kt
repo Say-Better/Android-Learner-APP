3cc0e5441f9404225ea6fc56041b4dd59a632d78
yeongin-ji <jyi0106@gachon.ac.kr>
2024-06-24 05:36:20 +0900

    ✨ Feat: 커밋 이슈연결 테스트
    #27

--- app/src/main/java/gdsc/solutionchallenge/saybetter/saybetterlearner/ui/videocall/VideoCallActivity.kt
+++ app/src/main/java/gdsc/solutionchallenge/saybetter/saybetterlearner/ui/videocall/VideoCallActivity.kt
@@ -142,6 +142,7 @@ class VideoCallActivity : ComponentActivity(), TTSListener {
         )
     }
 
+
     private fun setupVideo(
         trackState: MutableState<VideoTrack?>,
         track: VideoTrack,
